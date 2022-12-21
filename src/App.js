04922e75d52944092f096a150de0265d5cf7e7fb
Prajakta <prajaktakamble42@gmail.com>
2022-12-21 14:09:28 +0530

    basicdemo

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+let name = "Prajakta";
+function App() {
+  return (
+    <>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <img src="" alt="" />
+      <div className="container">
+        <h1>Hello {name} </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum,
+          voluptatibus, maiores doloremque ab aperiam dolorum commodi fuga saepe
+          quod ex cum ratione nesciunt eveniet culpa a error, minima neque
+          suscipit!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+//<></> --->jsx fragment
